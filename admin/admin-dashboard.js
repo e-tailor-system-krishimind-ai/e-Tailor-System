334e349ed11e1251e979cbb090a42235a4ea5c28
Krishna Kiran Aher <aherkrishna04@gmail.com>
2026-06-16 14:48:35 +0530

    Structured

---
+++ admin/admin-dashboard.js
@@ -0,0 +1,23 @@
+document.querySelectorAll(".sidebar li").forEach(item => {
+  item.addEventListener("click", () => {
+    document.querySelectorAll(".sidebar li")
+      .forEach(li => li.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
+
+const links = document.querySelectorAll(".sidebar a");
+const currentPage = window.location.pathname.split("/").pop();
+
+links.forEach(link => {
+  if (link.getAttribute("href") === currentPage) {
+    link.parentElement.classList.add("active");
+  }
+});
+
+
+
+function logout() {
+    localStorage.removeItem("user");
+    window.location.href = "../landing/newlanding_pg.html";
+}
